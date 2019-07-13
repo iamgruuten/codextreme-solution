13d13407af3ac77f6689e27a003236c6fcf509c4
WeedleQs <jasonleequansheng@gmail.com>
2019-07-13 11:08:56 +0800

    invalid system

---
+++ MyApplication/app/src/main/java/com/example/appdevin/myapplication/Class/Reward.java
@@ -0,0 +1,16 @@
+package com.example.appdevin.myapplication.Class;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.appdevin.myapplication.R;
+
+public class Reward extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reward);
+
+    }
+}
